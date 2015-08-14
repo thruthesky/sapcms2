e61d5f23e4ab0df1384a0f78015c9412a200a6ff
thruthesky <thruthesky@gmail.com>
2015-08-14 19:04:38 +0900

    Updating bulk message

--- module/smsgate/smsgate.hook.php
+++ module/smsgate/smsgate.hook.php
@@ -3,7 +3,8 @@ function hook_admin_menu_smsgate(&$variable=[]){
     echo "
 <li data-role='list-divider'>SMSGate Management</li>
 <li><a href='/smsgate/list/queue'>Queue  List</a></li>
-<li><a href='/smsgate/list/sent'>Sent List</a></li>
+<li><a href='/smsgate/list/sent'>Success List</a></li>
+<li><a href='/smsgate/list/fail'>Failure List</a></li>
 <li><a href='/smsgate/send'>Send an SMS</a></li>
 ";
 }
--- module/smsgate/template/smsgate.sent.html.php
+++ module/smsgate/template/smsgate.sent.html.php
@@ -5,5 +5,12 @@
 <H1>SENT!!</H1>
 
 <?php
-
 di($variables['scheduled']);
+?>
+
+<H1>ERROR</H1>
+
+
+<?php
+di($variables['error_number']);
+?>
