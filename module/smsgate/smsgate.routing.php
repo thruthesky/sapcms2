7294ef47f8889fc39dbf5faeb09c7de19dba1abf
ontuebenjamin <ontuebenjamin@gmail.com>
2015-08-14 20:55:08 +0800

    my changes

--- module/smsgate/smsgate.routing.php
+++ module/smsgate/smsgate.routing.php
@@ -3,6 +3,9 @@ use sap\src\Route;
 
 Route::add('/smsgate/send', "smsgate\\smsgate\\send");
 Route::add('/smsgate/list/queue', "smsgate\\smsgate\\queue");
+Route::add('/smsgate/list/success', "smsgate\\smsgate\\success");
+Route::add('/smsgate/list/fail', "smsgate\\smsgate\\fail");
+Route::add('/smsgate/statistics', "smsgate\\smsgate\\statistics");
 Route::add('/smsgate/delete', "smsgate\\smsgate\\delete");
 Route::add('/smsgate/sender/load', "smsgate\\smsgate\\sender_load_sms_from_queue");
 Route::add('/smsgate/sender/result', "smsgate\\smsgate\\sender_record_result");
---
+++ module/smsgate/template/smsgate.success.html.php
@@ -0,0 +1,72 @@
+<?php
+use sap\src\HTML;
+$no_item = sysconfig(NO_ITEM);
+$no_page = sysconfig(NO_PAGE);
+
+$total_record = $smses = entity(SMS_SUCCESS)->count();
+
+$page_no = page_no();
+
+$from = ($page_no-1) * $no_item;
+$smses = entity(SMS_SUCCESS)->rows("limit $from, $no_item");
+
+
+
+
+?>
+<h1>
+    SMSGate Message Success
+</h1>
+
+<?php if( !empty( $variables['notice'] ) ){?>
+    <div class='notice <?php echo $variables['notice']['type'] ?>'>
+        <?php echo $variables['notice']['type'] ?> : <?php echo $variables['notice']['message'] ?>
+    </div>
+<?php }?>
+
+
+
+<table data-role="table" id="table-module-list" data-mode="columntoggle" class="ui-responsive table-stroke">
+    <thead>
+    <tr>
+        <th data-priority="5">No.</th>
+        <th data-priority="3">Created</th>
+        <th data-priority="4">Priority</th>
+        <th>Number</th>
+        <th data-priority="1">Message</th>
+        
+    </tr>
+    </thead>
+    <tbody>
+
+    <?php
+	//di( $_GET );exit;
+	
+    foreach( $smses as $sms ) {
+
+        echo "<tr>";
+        echo "<td><a href='#'>$sms[idx]</a></td>";
+        echo "<td>" . date("m/d H:i", $sms['created']) . "</td>";
+        echo "<td>$sms[priority]</td>";
+        echo "<td>$sms[number]</td>";
+        echo "<td>$sms[message]</td>";
+        
+        echo "</tr>";
+    }
+    ?>
+    </tbody>
+</table>
+
+
+    <style>
+        nav.navigation-bar a {
+            display:inline-block;
+            margin:0 1px;
+            padding:4px 6px;
+            background-color: #d3e8f4;
+            border-radius: 2px;
+        }
+    </style>
+<?php
+echo HTML::paging($page_no, $total_record, $no_item, $no_page, null, null, '/smsgate/list/queue');
+
