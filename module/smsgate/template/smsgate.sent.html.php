e855d903a24a86da38b350050bad149d952faf6a
ontuebenjamin <ontuebenjamin@gmail.com>
2015-08-17 13:05:49 +0800

    text changes

--- module/smsgate/template/smsgate.sent.html.php
+++ module/smsgate/template/smsgate.sent.html.php
@@ -7,7 +7,7 @@
 <?php
 	foreach( $variables['scheduled'] as $schedule ){
 	
-		echo "<div>$schedule[message] --> $schedule[number]</div>";
+		echo "<div>$schedule[message] => $schedule[number]</div>";
 	
 	}
 ?>
