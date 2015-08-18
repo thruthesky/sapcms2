9d07d7ee39a9030dbf00f5dce23a4cabd7c5e1e5
ontuebenjamin <ontuebenjamin@gmail.com>
2015-08-18 15:00:16 +0800

    time

--- module/smsgate/template/smsgate.menu.html.php
+++ module/smsgate/template/smsgate.menu.html.php
@@ -1,3 +1,4 @@
+<div>Current Timezone [ <?php echo date_default_timezone_get()?> ] | <?php echo date("Y-m-d H:i:s",time()) ?></div>
 
 <a class='ui-btn ui-btn-inline' href='/smsgate/list/queue'>Queue  List</a>
 <a class='ui-btn ui-btn-inline' href='/smsgate/list/success'>Success List</a>
