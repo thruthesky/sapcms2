47f095a24cfd0e1635f46491ac58764ba49b94da
ontuebenjamin <ontuebenjamin@gmail.com>
2015-09-03 20:36:12 +0800

    changes

---
+++ module/statistics/template/statistics.admin.post.menu.html.php
@@ -0,0 +1,6 @@
+<a class='ui-btn ui-btn-inline' href="/admin/statistics">Overall Statistics</a>
+<a class='ui-btn ui-btn-inline' href="/admin/statistics/post?list_type=idx_root">Most Comments by root post</a>
+<a class='ui-btn ui-btn-inline' href="/admin/statistics/post?list_type=idx_root&group_by=idx_user">Most Comments by user</a>
+<a class='ui-btn ui-btn-inline' href="/admin/statistics/post?list_type=idx_root&group_by=idx_config">Most Comments by idx_config</a>
+<a class='ui-btn ui-btn-inline' href="/admin/statistics/post?list_type=idx_user">Most User Posts</a>
+<a class='ui-btn ui-btn-inline' href="/admin/statistics/post?list_type=idx_config">Most Config Posts</a>
