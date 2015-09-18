a86ee9c34b5673490a36be1e9909d66d42082357
ontuebenjamin <ontuebenjamin@gmail.com>
2015-09-18 19:13:02 +0800

    app design fixes

--- module/app/template/page.postList.comments.html.php
+++ module/app/template/page.postList.comments.html.php
@@ -2,18 +2,18 @@
 if ( ! isset($post['comments']) ) return;
 $comments = &$post['comments'];
 foreach ( $comments as $comment ) {
+	$idx_user = $comment['idx_user'];
+	if( $idx_user == 0 ) $idx_user = 1;	
+
     $depth = $comment['depth'];
     if ( $depth > 5 )  $indent = ( 3 * 12 ) + ( $depth - 4 ) * 6;
     else  $indent = ( $depth - 1 ) * 12;//$padding = 12;
     //$indent = ( $depth - 1 ) * $padding;
+	$date = date("d M Y",$comment['created']);
+	
+	$edit_url = url_post_comment_edit($comment['idx']);
     ?>
     <div id="comment<?php echo $comment['idx']?>" class="comment" idx="<?php echo $comment['idx']?>" depth='<?php echo $depth; ?>' style="margin-left:<?php echo $indent; ?>px">
-        <nav class="menu">
-            글번호 : <?php echo $comment['idx']?>
-            <a href="<?php echo url_post_comment_edit($comment['idx'])?>">수정</a>
-            <a href="<?php echo url_post_delete($comment['idx'])?>">삭제</a>
-        </nav>
-
         <?php
 /*<?php widget('post_view_vote', ['post'=>$comment])?>
 */?>
@@ -34,9 +34,34 @@ foreach ( $comments as $comment ) {
         </div>
 
 
-        <div class="comment-reply-button">
-            [Reply...]
-        </div>
+       <nav class='user-command comment-command'>
+			<div class="comment-reply-button">
+				Reply
+				<img src='<?php echo sysconfig(URL_SITE) ?>module/app/img/blue_dot.png'/>
+			</div>  
+			<nav class="vote" idx="<?php echo $post['idx']?>">				
+				<div class="good">
+					<?php 					
+					if( $comment['no_vote_good'] > 0 ) echo $comment['no_vote_good']; ?>
+					Like<?php echo $comment['no_vote_good'] <= 1 ? "" : "s"?>
+				</div>
+				<img src='<?php echo sysconfig(URL_SITE) ?>module/app/img/blue_dot.png'/>
+			</nav>
+			<?php if( $idx_user == login('idx') ){ ?>
+			<div class="edit">
+				<a href="<?php echo url_post_comment_edit($comment['idx'])?>">Edit</a>
+				<img src='<?php echo sysconfig(URL_SITE) ?>module/app/img/blue_dot.png'/>
+			</div>
+			<?php }else{ ?>
+				<div class="edit">
+					<a href="#">Report</a>
+					<img src='<?php echo sysconfig(URL_SITE) ?>module/app/img/blue_dot.png'/>
+				</div>
+			<?php }?>
+			<div class="date">
+			   <?php echo $date?>
+			</div>	
+		</nav>
         <div class="comment-form" style="display:none;">
             <?php include template('page.postList.comment-form') ?>
         </div>
