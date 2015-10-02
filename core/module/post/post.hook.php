1540694263c83052621ce8ca6d453afd92767de7
thruthesky <thruthesky@gmail.com>
2015-10-02 14:10:08 +0900

    Updating app. alert & dialogs - user update - photo update

--- core/module/post/post.hook.php
+++ core/module/post/post.hook.php
@@ -1,6 +1,8 @@
 <?php
 
 function hook_admin_menu_post(&$variable=[]){
+
+
     echo "
       <li data-role='list-divider'>POST MODULE Management</li>
       <li><a href='/admin/post/config/list'>POST List</a></li>
