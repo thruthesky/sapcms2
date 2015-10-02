f2a7c07251771765f5a28edbea60e196beb6e168
ontuebenjamin <ontuebenjamin@gmail.com>
2015-10-02 13:54:04 +0800

    working on message

--- core/module/message/message.routing.php
+++ core/module/message/message.routing.php
@@ -1,4 +1,5 @@
 <?php
 
+route('/message/list', 'message\\Message\\collection');
 route('/message/send', 'message\\Message\\send');
 route('/message/create', 'message\\Message\\messageCreate');
--- core/module/message/src/Message.php
+++ core/module/message/src/Message.php
@@ -11,12 +11,22 @@ class Message extends Entity {
     public function __construct() {
         parent::__construct(MESSAGE_TABLE);
     }
-
+	
+	public static function collection() {
+		$data = [];
+	
+		$data['messages'] = message()->rows("idx_to = ".login('idx'));
+		$data['template'] = 'message.list';
+		Response::render($data);
+	}
+	
     public static function messageCreate() {
 		Response::render();
     }
 
     public static function send() {
+		$data = [];
+	
 		$user_to = User()->load( 'id',request('user_id_to') );
 		if( !empty( $user_to ) ) $idx_to = $user_to->idx;
 		else{
@@ -32,9 +42,5 @@ class Message extends Entity {
 		$data['template'] = 'message.messageCreate';
 		//$data['page'] = 'send';
         Response::render($data);
-    }
-
-
-    
-
+    }	
 }
