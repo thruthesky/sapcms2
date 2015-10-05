41bc7890439aecdc4192c4a285ba42c90b90c52d
ontuebenjamin <ontuebenjamin@gmail.com>
2015-10-05 15:04:59 +0800

    name to id

--- core/module/post/widget/post_list_body/post_list_body.php
+++ core/module/post/widget/post_list_body/post_list_body.php
@@ -15,7 +15,7 @@ $posts = post()->postListData();
             <tr class="row">
                 <td class="no"><a href="<?php echo url_post_view($post)?>"><span><?php echo $post['idx']?></span></a></td>
                 <td class="title"><a href="<?php echo url_post_view($post)?>"><span><?php widget('post_list_title', ['post'=>$post]); ?></span></a></td>
-                <td class="name"><a href="<?php echo url_post_view($post)?>"><span><?php echo $post['user']['name']?></span></a></td>
+                <td class="name"><a href="<?php echo url_post_view($post)?>"><span><?php echo $post['user']['id']?></span></a></td>
                 <td class="view"><a href="<?php echo url_post_view($post)?>"><span><?php echo $post['no_view'];?></span></a></td>
                 <td class="date"><a href="<?php echo url_post_view($post)?>"><span><?php echo date_short($post['created']);?></span></a></td>
             </tr>
