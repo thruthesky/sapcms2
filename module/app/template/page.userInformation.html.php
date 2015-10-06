03ca6d6fe5d19a603afacc9c7a05aece88fa20f7
ontuebenjamin <ontuebenjamin@gmail.com>
2015-10-06 21:27:54 +0800

    app changes

---
+++ module/app/template/page.userInformation.html.php
@@ -0,0 +1,24 @@
+<?php
+	$photo = $user->getPrimaryPhoto();
+	if ( $photo ) $url = $photo->urlThumbnail(140, 140);
+	else if( empty( $url ) ) $url = sysconfig(URL_SITE)."module/app/img/register_logo.png";	
+	
+	//$date = date("y-m-d",$user['created']);
+	$date =  date("y-m-d",$user->created);
+?>
+<table class='table-user-info' cellpadding=0 cellspacing=0 width='100%'>
+	<tr valign='top'>
+		<td>
+			<div class='primary-photo'>
+				<img src='<?php echo $url;?> '/>
+			</div>
+		</td>
+		<td width='99%'>
+			<div class='info'>
+				<div class='id'><?php echo $user->id; ?></div>
+				<div class='name'><?php echo $user->name; ?></div>
+				<div class='name'><?php echo $date; ?></div>			
+			</div>
+		</td>
+	</tr>
+</table>
