39dd1c5cda712db695a6ceb116d7aa17f2e6692c
ontuebenjamin <ontuebenjamin@gmail.com>
2015-10-14 21:32:02 +0800

    temporary wooreeedu_clean

---
+++ theme/wooreeedu_clean/template/front.content.text-with-thumbnail.html.php
@@ -0,0 +1,44 @@
+<?php
+$image_fhm = post()->getLatestPostImage(7);
+$src_fhm = $image_fhm->urlThumbnail(45, 45);
+$fhm = post_data($image_fhm->idx_target);
+
+$image_news = post()->getLatestPostImage(8);
+$src_news = $image_news->urlThumbnail(45, 45);
+$news = post_data($image_news->idx_target);
+if( $image_news->idx_target == 0 ) return;
+
+if( empty( $news ) ) return;
+if( empty( $fhm ) ) return;
+?>
+<div class="text-with-thumbnail">
+    <a href="<?php echo $fhm->url()?>">
+        <table width="100%" cellspacing="0" cellpadding="0">
+            <tr>
+                <td width="5%"><img class='thumbnail' src="<?php echo $src_fhm?>"></td>
+                <td width="93%">
+                    <div class="content">
+                        <h3><?php echo $fhm->getTitle(64) ?></h3>
+                        <div class="comment"><?php echo $fhm->getDescription(64) ?></div>
+                    </div>
+                </td>
+                <td width="2%"><img src="/theme/default/tmp/fhm.png"</td>
+            </tr>
+        </table>
+    </a>
+
+    <a href="<?php echo $news->url()?>">
+        <table width="100%" cellspacing="0" cellpadding="0">
+            <tr>
+                <td width="5%"><img class='thumbnail' src="<?php echo $src_news?>"></td>
+                <td width="93%">
+                    <div class="content">
+                        <h3><?php echo $news->getTitle(64) ?></h3>
+                        <div class="comment"><?php echo $news->getDescription(64) ?></div>
+                    </div>
+                </td>
+                <td width="2%"><img src="/theme/default/tmp/news2.png"</td>
+            </tr>
+        </table>
+    </a>
+</div>
--- theme/wooreeedu_clean/template/front.page.html.php
+++ theme/wooreeedu_clean/template/front.page.html.php
@@ -4,6 +4,8 @@ add_css();
 <div class="front-top-banner">
 	<img src='theme/wooreeedu_clean/img/top-banner.jpg'/>
 </div>
-<div class='page-content'>
-	CONTENT HERE
+<div class='front-content'>
+	<?php include template('front.content.text-with-thumbnail') ?>
+	<?php include template('front.content.text') ?>
+	<?php include template('front.content.text-photo') ?>
 </div>
