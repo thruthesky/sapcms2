857035ef580e996ecec075efe2a73bec411e0bde
ontuebenjamin <ontuebenjamin@gmail.com>
2015-10-14 18:24:05 +0800

    mobile app updates

--- module/app/template/page.postList.postForm.html.php
+++ module/app/template/page.postList.postForm.html.php
@@ -22,36 +22,36 @@ if( empty( $no_comment_form ) ) $no_comment_form = 0;
 	if( !empty( $post['idx'] ) ) echo "<input type='hidden' name='idx' value='$post[idx]' />";
 	?>
 	<?php echo html_hidden_post_variables(); ?>
-    <table width="100%" cellpadding="0" cellspacing="0">
-            <tr valign="top">
-				<td class='td-primary-photo' width="45">                    
-					<?php if( !empty( $url_primary_photo ) ){?>
-						<div class='primary-photo comment-photo'><img src='<?php echo $url_primary_photo; ?>'/></div>
-					<?php } else {?>
-						<div class='primary-photo comment-photo temp'><img src='<?php echo sysconfig(URL_SITE) ?>module/app/img/no_primary_photo.png'/></div>
+    <table class='table-post-form' width="100%" cellpadding="0" cellspacing="0">
+		<tr valign="top">
+			<td class='td-primary-photo' width="45">                    
+				<?php if( !empty( $url_primary_photo ) ){?>
+					<div class='primary-photo comment-photo'><img src='<?php echo $url_primary_photo; ?>'/></div>
+				<?php } else {?>
+					<div class='primary-photo comment-photo temp'><img src='<?php echo sysconfig(URL_SITE) ?>module/app/img/no_primary_photo.png'/></div>
+				<?php }?>
+			</td>
+			<td width="99%">
+				<textarea class='post-form-content' name="content" placeholder="Write a post."><?php if( !empty( $post['content'] ) ) echo $post['content']; ?></textarea>
+			</td>                
+		</tr>
+	</table>
+	<table class='show-on-click table-post-buttons' width="100%" cellpadding="0" cellspacing="0">
+		<tr valign='top'>								
+			<td width='99%'>
+				<img class='post-loader' src='<?php echo sysconfig(URL_SITE) ?>module/app/img/loader5.gif'/>
+			</td>
+			<td width="1%">
+				<div style='width:<?php echo $buttons_width; ?>px'>
+					<img class="post-file-upload-button" src="<?php echo sysconfig(URL_SITE)?>module/app/img/camera_white_temp.png">
+					<input class="form-comment-add-submit" type="submit" value="POST">
+					<?php if( !empty( $edit_mode ) ) {?>
+						<div class='post-cancel' type='post'>Cancel</div>
 					<?php }?>
-                </td>
-				<td width="99%">
-                    <textarea class='post-form-content' name="content" placeholder="Write a post."><?php if( !empty( $post['content'] ) ) echo $post['content']; ?></textarea>
-                </td>                
-            </tr>
-        </table>
-		<table class='show-on-click' width="100%" cellpadding="0" cellspacing="0">
-			<tr valign='top'>								
-				<td width='99%'>
-					<img class='post-loader' src='<?php echo sysconfig(URL_SITE) ?>module/app/img/loader5.gif'/>
-				</td>
-				<td width="1%">
-					<div style='width:<?php echo $buttons_width; ?>px'>
-						<img class="post-file-upload-button" src="<?php echo sysconfig(URL_SITE)?>module/app/img/camera_white_temp.png">
-						<input class="form-comment-add-submit" type="submit" value="POST">
-						<?php if( !empty( $edit_mode ) ) {?>
-							<div class='post-cancel' type='post'>Cancel</div>
-						<?php }?>
-					</div>
-				</td>
-			</tr>
-		</table>
-        <div class='file-display files'>
-		</div>
+				</div>
+			</td>
+		</tr>
+	</table>
+	<div class='file-display files'>
+	</div>
 </form>
