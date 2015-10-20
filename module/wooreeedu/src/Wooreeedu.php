3ea764919914b0be92d93a7958b932be9f7020c3
ontuebenjamin <ontuebenjamin@gmail.com>
2015-10-20 18:59:46 +0800

    module changes

--- module/wooreeedu/src/Wooreeedu.php
+++ module/wooreeedu/src/Wooreeedu.php
@@ -5,10 +5,18 @@ use sap\src\Response;
 use sap\core\message\Message;
 
 class Wooreeedu {
-	public static function schedule(){
+	public static function course(){
 		return Response::render([
-            'template'=>'schedule',
-            'page'=>'schedule',
+            'template'=>'course',
+            'page'=>'course',
+            //'config' => $config,
+        ]);
+	}
+	
+	public static function lecture(){
+		return Response::render([
+            'template'=>'lecture',
+            'page'=>'course',
             //'config' => $config,
         ]);
 	}
@@ -16,8 +24,145 @@ class Wooreeedu {
 	public static function trainingCost(){
 		return Response::render([
             'template'=>'trainingCost',
-            'page'=>'cost',
+            'page'=>'course',
             //'config' => $config,
         ]);
 	}
+	
+	public static function junior(){
+		return Response::render([
+            'template'=>'junior',
+            'page'=>'junior',
+            //'config' => $config,
+        ]);
+	}
+	
+	public static function juniorEarlyStudy(){
+		return Response::render([
+            'template'=>'juniorEarlyStudy',
+            'page'=>'junior',
+            //'config' => $config,
+        ]);
+	}
+	
+	public static function advantagesInPH(){
+		return Response::render([
+            'template'=>'advantagesInPH',
+            'page'=>'junior',
+            //'config' => $config,
+        ]);
+	}
+	
+	public static function earlyCost(){
+		return Response::render([
+            'template'=>'earlyCost',
+            'page'=>'junior',
+            //'config' => $config,
+        ]);
+	}
+	
+	public static function recommended(){
+		return Response::render([
+            'template'=>'recommended',
+            'page'=>'junior',
+            //'config' => $config,
+        ]);
+	}
+	
+	public static function camp(){
+		return Response::render([
+            'template'=>'camp',
+            'page'=>'camp',
+            //'config' => $config,
+        ]);
+	}
+	public static function campInformation(){
+		return Response::render([
+            'template'=>'campInformation',
+            'page'=>'camp',
+            //'config' => $config,
+        ]);
+	}
+	public static function schedule(){
+		return Response::render([
+            'template'=>'schedule',
+            'page'=>'camp',
+            //'config' => $config,
+        ]);
+	}
+	public static function preparation(){
+		return Response::render([
+            'template'=>'preparation',
+            'page'=>'camp',
+            //'config' => $config,
+        ]);
+	}
+	public static function weekendActivities(){
+		return Response::render([
+            'template'=>'weekendActivities',
+            'page'=>'camp',
+            //'config' => $config,
+        ]);
+	}
+	public static function requirement(){
+		return Response::render([
+            'template'=>'requirement',
+            'page'=>'camp',
+            //'config' => $config,
+        ]);
+	}
+	
+	public static function gallery(){
+		return Response::render([
+            'template'=>'gallery',
+            'page'=>'gallery',
+            //'config' => $config,
+        ]);
+	}
+	public static function popsong(){
+		return Response::render([
+            'template'=>'popsong',
+            'page'=>'gallery',
+            //'config' => $config,
+        ]);
+	}
+	public static function tripping(){
+		return Response::render([
+            'template'=>'tripping',
+            'page'=>'gallery',
+            //'config' => $config,
+        ]);
+	}
+	public static function speech(){
+		return Response::render([
+            'template'=>'speech',
+            'page'=>'gallery',
+            //'config' => $config,
+        ]);
+	}
+	public static function xmas(){
+		return Response::render([
+            'template'=>'xmas',
+            'page'=>'gallery',
+            //'config' => $config,
+        ]);
+	}
+	public static function seminar(){
+		return Response::render([
+            'template'=>'seminar',
+            'page'=>'gallery',
+            //'config' => $config,
+        ]);
+	}
+	
+	public static function videoEnglish(){
+		return Response::render([
+            'template'=>'videoEnglish',
+            'page'=>'videoEnglish',
+            //'config' => $config,
+        ]);
+	}
+	
+	
+	
 }
--- module/wooreeedu/wooreeedu.routing.php
+++ module/wooreeedu/wooreeedu.routing.php
@@ -1,5 +1,29 @@
 <?php
 use sap\src\Route;
 
-Route::add('/schedule', 'wooreeedu\\Wooreeedu\\schedule');
-Route::add('/trainingCost', 'wooreeedu\\Wooreeedu\\trainingCost');
+Route::add('/course', 'wooreeedu\\Wooreeedu\\course');
+Route::add('/course/lecture', 'wooreeedu\\Wooreeedu\\lecture');
+Route::add('/course/trainingCost', 'wooreeedu\\Wooreeedu\\trainingCost');
+
+Route::add('/junior', 'wooreeedu\\Wooreeedu\\junior');
+Route::add('/junior/juniorEarlyStudy', 'wooreeedu\\Wooreeedu\\juniorEarlyStudy');
+Route::add('/junior/advantagesInPH', 'wooreeedu\\Wooreeedu\\advantagesInPH');
+Route::add('/junior/earlyCost', 'wooreeedu\\Wooreeedu\\earlyCost');
+Route::add('/junior/recommended', 'wooreeedu\\Wooreeedu\\recommended');
+
+Route::add('/camp', 'wooreeedu\\Wooreeedu\\camp');
+Route::add('/camp/campInformation', 'wooreeedu\\Wooreeedu\\campInformation');
+Route::add('/camp/schedule', 'wooreeedu\\Wooreeedu\\schedule');
+Route::add('/camp/preparation', 'wooreeedu\\Wooreeedu\\preparation');
+Route::add('/camp/weekendActivities', 'wooreeedu\\Wooreeedu\\weekendActivities');
+Route::add('/camp/requirement', 'wooreeedu\\Wooreeedu\\requirement');
+
+Route::add('/gallery', 'wooreeedu\\Wooreeedu\\gallery');
+Route::add('/gallery/popsong', 'wooreeedu\\Wooreeedu\\popsong');
+Route::add('/gallery/tripping', 'wooreeedu\\Wooreeedu\\tripping');
+Route::add('/gallery/speech', 'wooreeedu\\Wooreeedu\\speech');
+Route::add('/gallery/xmas', 'wooreeedu\\Wooreeedu\\xmas');
+Route::add('/gallery/seminar', 'wooreeedu\\Wooreeedu\\seminar');
+
+Route::add('/ve', 'wooreeedu\\Wooreeedu\\videoEnglish');
+
