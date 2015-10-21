2f8b4c807267822b3d1d847c04f4b8e6d881caa1
ontuebenjamin <ontuebenjamin@gmail.com>
2015-10-21 15:50:19 +0800

    anon message

--- core/module/message/src/Message.php
+++ core/module/message/src/Message.php
@@ -102,6 +102,7 @@ class Message extends Entity {
 				$idx_to = $user_to->idx;
 				//if not error
 				$idx_from = login('idx');
+				if( empty( $idx_from ) ) $idx_from = 0;
 				$message_entity = message()->set('idx_from',$idx_from)
 				->set('idx_to',$idx_to)
 				->set('title', request('title'))
