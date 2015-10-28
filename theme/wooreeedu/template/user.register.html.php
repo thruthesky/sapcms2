8ba4dc49b7bb934d377468ff5c8a15ba37c69360
ontuebenjamin <ontuebenjamin@gmail.com>
2015-10-28 17:24:08 +0800

    post and theme for post

--- theme/wooreeedu/template/user.register.html.php
+++ theme/wooreeedu/template/user.register.html.php
@@ -4,7 +4,15 @@
 
 <div class='member-form-wrapper register wooreeedu'>
 	<div class='title'>Register</div>
-	<form class='member-register-form member-form' method="post">
+	<form class="member-register-form member-form ajax-file-upload" method="post" enctype="multipart/form-data">
+		<input type="hidden" name="file_module" value="user">
+        <input type="hidden" name="file_type" value="primary_photo">
+        <input type="hidden" name="file_idx_target" value="<?php echo login('idx')?>">
+        <input type="hidden" name="file_unique" value="1">
+        <input type="hidden" name="file_finish" value="1">
+        <input type="hidden" name="file_image_thumbnail_width" value="140">
+        <input type="hidden" name="file_image_thumbnail_height" value="140">
+        <input type="hidden" name="file_callback" value="callback_user_primary_photo_upload">
 	<?php
 
 		if( !empty( $variables['user'] ) ){
@@ -16,6 +24,10 @@
 	<?php if ( $user = login() ) { ?>
 
 		<?php
+		$file_upload_form_name = 'primary_photo';
+		$form_upload_single_file = true;
+		include template('user.photo');
+		
 		$name = $user->get('name');
 		$mail = $user->get('mail');
 		if( !empty( $variables['user'] ) ) $user = $variables['user'];
