c9d4b6931ca1bfed61807737f92207039d673403
ontuebenjamin <ontuebenjamin@gmail.com>
2015-10-29 13:02:32 +0800

    post edit

--- core/module/post/widget/post_list_menu/post_list_menu.php
+++ core/module/post/widget/post_list_menu/post_list_menu.php
@@ -2,9 +2,9 @@
 $user_id = login('id');
 ?>
 <div class="buttons right">
-<a href="<?php echo url_post_create();?>">Write a post</a>
+<a class='write' href="<?php echo url_post_create();?>">Write a post</a>
 <?php if( $user_id == 'admin' ){ ?>
-<a href="<?php echo url_post_config();?>">Settings</a>
+<a class='settings' href="<?php echo url_post_config();?>">Settings</a>
 <?php } ?>
 </div>
 
