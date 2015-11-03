605e89f513935c6fef83a15ab15309f7e3634fe5
ontuebenjamin <ontuebenjamin@gmail.com>
2015-11-03 14:23:15 +0800

    app header

--- module/app/template/page.header.html.php
+++ module/app/template/page.header.html.php
@@ -16,11 +16,11 @@ if( !empty( $idx_user ) ){
 <div id="page-header">
     <ul id="main-menu">
         <li class='link logo' route="front_page"><span class="sprite logo"></span><div class='label'>우리에듀</div></li>
-        <li class='link sub-menu ask' route="introduction"><span class="sprite ask"></span><div class='label'>인사말</div></li>
+        <li class='link sub-menu ask' route='pageView' page="introduction"><span class="sprite ask"></span><div class='label'>인사말</div></li>
         <li class='link sub-menu forum' route="postList" post_id="qna"><span class="sprite forum"></span><div class='label'>질문과답변</div></li>
-        <li class='link sub-menu news' route="postList" post_id="news"><span class="sprite news"></span><div class='label'>News</div></li>
-        <li class='link sub-menu market' route="postList" post_id="gallery"><span class="sprite market"></span><div class='label'>Gallery</div></li>
-        <li class='link sub-menu jobs' route="postList" post_id="course"><span class="sprite jobs"></span><div class='label'>Course</div></li>
+        <li class='link sub-menu news' route="postList" post_id="wooreeedu"><span class="sprite news"></span><div class='label'>Posts</div></li>
+        <li class='link sub-menu market' route="pageView" page="gallery"><span class="sprite market"></span><div class='label'>Gallery</div></li>
+        <li class='link sub-menu jobs' route="pageView" page="course"><span class="sprite jobs"></span><div class='label'>Course</div></li>
 		<li class='link sub-menu my_page' route="<?php echo $my_page_url; ?>"><span class="sprite my_page"><?php echo $post_primary_photo; ?></span><div class='label'><?php echo $my_page_text; ?></div></li>
         <li class='menu show-panel'><span class="sprite menu"></span><div class='label'>Menu</div></li>
     </ul>
