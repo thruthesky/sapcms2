5d3eec4f7ceb72d4dfe3ab5689452cb94ca737f7
ontuebenjamin <ontuebenjamin@gmail.com>
2015-11-06 20:50:57 +0800

    module

--- module/app/template/page.login.html.php
+++ module/app/template/page.login.html.php
@@ -7,19 +7,19 @@
 			<div class='bg-wrapper'>
 				<div class='sprite user'></div>
 			</div>
-            <input type="text" name="id" value="" placeholder="User ID">
+            <input type="text" name="id" value="" placeholder="아이디 입력">
         </div>
         <div class='input-wrapper password'>
 			<div class='bg-wrapper'>
 				<div class='sprite password'></div>
 			</div>
-            <input type="password" name="password" value="" placeholder="Password">
+            <input type="password" name="password" value="" placeholder="비밀번호 입력">
         </div>
         <div class='input-wrapper'>
-            <input type="submit" value="Log In">
+            <input type="submit" value="로그인">
         </div>
 		<div class='input-wrapper'>
-			<span class="link register" route="register">Create an Account</span>
+			<span class="link register" route="register">회원가입</span>
 		</div>
 		<div class='forgot-password'>Forgot Password?</div>
     </form>
