dca2fc28b52ab0a6a422e7db100679d3f67612d8
thruthesky <thruthesky@gmail.com>
2015-11-11 23:24:39 +0900

    Translating to Korean

--- theme/englishworld/template/header.html.php
+++ theme/englishworld/template/header.html.php
@@ -107,10 +107,11 @@ add_css('featured.item.css');
 							<td width='16.67%'>
 								<div class='item'>
 									<ul>
-										<li><a href="/gallery/2">기초 영어 회화</a></li>
-										<li><a href="/gallery/3">비지니스 영어</a></li>
-										<li><a href="/gallery/4">인터뷰 영어</a></li>
-										<li><a href="/gallery/5">어린이 영어</a></li>
+										<li><a href="/gallery/1">교재안내</a></li>
+										<li><a href="/gallery/2">파닉스 영어</a></li>
+										<li><a href="/gallery/3">어린이 영어</a></li>
+										<li><a href="/gallery/4">기초 영어 회화</a></li>
+										<li><a href="/gallery/5">비지니스 영어</a></li>
 										<li><a href="/gallery/6">시험 영어</a></li>
 									</ul>
 								</div>
