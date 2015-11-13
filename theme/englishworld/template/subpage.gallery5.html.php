596fed06bfe12d0a006a32c2d9df7f3b0fbf0be7
ontuebenjamin <ontuebenjamin@gmail.com>
2015-11-13 14:30:40 +0800

    added spacing between text and images

--- theme/englishworld/template/subpage.gallery5.html.php
+++ theme/englishworld/template/subpage.gallery5.html.php
@@ -5,7 +5,7 @@
 	<div class='title'><span>비지니스 영어</span></div>
 	<div class='group'>
 		비즈니스 영어 교재 안내입니다.<br>
-		업무 전화, 회의, 이메일 문서 교환 등의 상황에서 일어 날 수 있는 내용을 소재로 하여 수업을 진행합니다.<br>
+		업무 전화, 회의, 이메일 문서 교환 등의 상황에서 일어 날 수 있는 내용을 소재로 하여 수업을 진행합니다.<br><br>
         <img src="/theme/englishworld/img/class/book-6.png"><br>
 	</div>
 </div>
--- theme/englishworld/template/subpage.gallery6.html.php
+++ theme/englishworld/template/subpage.gallery6.html.php
@@ -5,7 +5,7 @@
 	<div class='title'><span>읽기 / 독해</span></div>
 	<div class='group'>
 		읽기(독해)와 스토리북에 과련된 영어 교재 안내입니다.<br>
-		읽기 교재는 주제에 따라서 프리토킹 교재로도 사용 될 수 있습니다.<br>
+		읽기 교재는 주제에 따라서 프리토킹 교재로도 사용 될 수 있습니다.<br><br>
 		<img src="/theme/englishworld/img/class/book-7.png"><br>
 	</div>
 </div>
