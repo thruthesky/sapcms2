9204fb831f65bdfdd297aac2027a8895b33331b6
ontuebenjamin <ontuebenjamin@gmail.com>
2015-11-17 20:37:38 +0800

    theme changs

--- theme/han/template/subpage.gallery6.html.php
+++ theme/han/template/subpage.gallery6.html.php
@@ -4,8 +4,10 @@
 <div class='temp-page <?php echo $page; ?>'>
 	<div class='title'><span>읽기 / 독해</span></div>
 	<div class='group'>
+		<div class='text-content'>
 		읽기(독해)와 스토리북에 과련된 영어 교재 안내입니다.<br>
-		읽기 교재는 주제에 따라서 프리토킹 교재로도 사용 될 수 있습니다.<br><br>
+		읽기 교재는 주제에 따라서 프리토킹 교재로도 사용 될 수 있습니다.
+		</div>
 		<img src="/theme/englishworld/img/class/book-7.png"><br>
 	</div>
 </div>
--- theme/han/template/subpage.junior1.html.php
+++ theme/han/template/subpage.junior1.html.php
@@ -6,7 +6,7 @@
 	<div class='group'>
 
         <img src="/theme/englishworld/img/class/free-1.jpg"><br>
-
+		<div class='text-content'>
         무료 체험 & 레벨테스트 상담전화 : <?php echo $phone_number?><br>
 
         무료 체험 수업은 레벨테스트라고 보시면됩니다.<br>
@@ -17,7 +17,7 @@
         레벨테스트의 성격이 강하며 혹시나 웹캠과 헤드셋에 문제가 있을 수 있으므로 그 테스트도 같이 하는 것입니다.<br>
 
         만약 무료 체험 수업에 문제가 있을 경우, 무료 체험 수업이 잘 될 때까지 계속 진행합니다.<br>
-
+		</div>
 
 	</div>
 </div>
