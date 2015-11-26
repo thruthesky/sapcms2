39160f459b7c1b9172c5f48d8741e54bc8875506
ontuebenjamin <ontuebenjamin@gmail.com>
2015-11-26 14:18:48 +0800

    daniel china

---
+++ theme/danielchina/template/subpage.course2.html.php
@@ -0,0 +1,9 @@
+<?php
+	extract( $variables );	
+?>
+<div class='temp-page <?php echo $page; ?>'>
+	<div class='title'><span><?php echo strtoupper( $title_text ); ?></span></div>
+	<div class='group'>
+		CONTENT HERE
+	</div>
+</div>
---
+++ theme/danielchina/template/subpage.junior1.html.php
@@ -0,0 +1,23 @@
+<?php
+	extract( $variables );	
+?>
+<div class='temp-page <?php echo $page; ?>'>
+	<div class='title'><span>무료 체험 수업</span></div>
+	<div class='group'>
+
+        <img src="/theme/danielchina/img/class/free-1.jpg"><br>
+		<div class='text-content'>
+        무료 체험 & 레벨테스트 상담전화 : <?php echo $phone_number?><br>
+
+        무료 체험 수업은 레벨테스트라고 보시면됩니다.<br>
+
+
+		무료 체험 수업은 모든 학생들이 처음 화상 영어 수업을 할 때 한번 또는 두번 하는 것으로
+
+        레벨테스트의 성격이 강하며 혹시나 웹캠과 헤드셋에 문제가 있을 수 있으므로 그 테스트도 같이 하는 것입니다.<br>
+
+        만약 무료 체험 수업에 문제가 있을 경우, 무료 체험 수업이 잘 될 때까지 계속 진행합니다.<br>
+		</div>
+
+	</div>
+</div>
